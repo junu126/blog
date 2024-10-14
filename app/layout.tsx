5024c43a5fa628ecbec2fdc2969ec6a2cf27e1e9
flex-junwoo <junwoo@flex.team>
2024-10-14 23:04:42 +0900

    refactor(folder): app내의 정적 파일들 외부로 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const pretendard = localFont({
-  src: "./fonts/PretendardVariable.woff2",
+  src: "../fonts/PretendardVariable.woff2",
   display: "swap",
   weight: "45 920",
   variable: "--font-pretendard",
