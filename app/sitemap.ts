43ac2380e98875ff7e164982fd26eff3bcc5ca07
flex-junwoo <junwoo@flex.team>
2024-10-14 21:42:21 +0900

    feat(icons): sitemap, robots 파일 작성

---
+++ app/sitemap.ts
@@ -0,0 +1,52 @@
+import { MetadataRoute } from "next";
+
+const ROOT_SITE_MAP: MetadataRoute.Sitemap[number] = {
+  url: "https://junukim.dev",
+  lastModified: new Date(),
+  changeFrequency: "daily",
+  priority: 0.5,
+};
+
+const CV_SITE_MAP: MetadataRoute.Sitemap[number] = {
+  url: "https://junukim.dev/cv",
+  lastModified: new Date(),
+  changeFrequency: "daily",
+  priority: 1,
+};
+
+const ARTICLE_SITE_MAP: MetadataRoute.Sitemap[number] = {
+  url: "https://junukim.dev/articles",
+  lastModified: new Date(),
+  changeFrequency: "daily",
+  priority: 0.8,
+};
+
+const ETC_SITE_MAP: MetadataRoute.Sitemap[number] = {
+  url: "https://junukim.dev/etc",
+  lastModified: new Date(),
+  changeFrequency: "daily",
+  priority: 0.5,
+};
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const articleSiteMap: MetadataRoute.Sitemap = [].map((_it) => ({
+    ...ARTICLE_SITE_MAP,
+    url: `https://junukim.dev/articles/${0}`, // FIXME: url 포함시키기
+    lastModified: new Date(), // FIXME: 포스팅 날짜 포함시키기
+  }));
+
+  const etcSiteMap: MetadataRoute.Sitemap = [].map((_it) => ({
+    ...ETC_SITE_MAP,
+    url: `https://junukim.dev/etc/${0}`, // FIXME: url 포함시키기
+    lastModified: new Date(), // FIXME: 포스팅 날짜 포함시키기
+  }));
+
+  return [
+    ROOT_SITE_MAP,
+    CV_SITE_MAP,
+    ARTICLE_SITE_MAP,
+    ...articleSiteMap,
+    ETC_SITE_MAP,
+    ...etcSiteMap,
+  ];
+}
