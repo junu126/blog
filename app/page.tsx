f49f973c67d480b5c5d8e9e56ad125315ad7e576
flex-junwoo <junwoo@flex.team>
2024-12-25 16:59:22 +0900

    fixup! feat(main): 메인 홈 페이지 헤더 작성

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,12 @@ export default async function Home() {
     >
       <PageHeader
         title="Junukim.dev"
-        description="Product engineer based in seoul, south korea"
+        description={
+          <>
+            "There is no magic to achievement. It's really about hard work,
+            choices and persistence."
+          </>
+        }
       >
         <div>
           <PageHeader.Text>
@@ -29,12 +34,11 @@ export default async function Home() {
           </PageHeader.Text>
           <br />
           <PageHeader.Text>
-            매일같이 마주하는 도전과 선택 속에서, 우리는 스스로를 발견하고
-            조금씩 성장하죠. 이곳은 제가 경험한 작은 성공과 실패, 그리고 그
-            속에서 배운 이야기들을 기록한 곳이에요. 또 누구에게나 실패는
-            아프지만, 그 속에 숨어 있는 배움은 더 큰 도약으로 이어질 수 있다고
-            믿어요. 이곳은 도전을 두려워하지 않고, 넘어짐 속에서 배운 것들을
-            기록한 공간이에요.
+            매일같이 마주하는 도전과 선택 속에서 우리는 스스로를 발견하고,
+            조금씩 성장해 나갑니다. 이곳은 제가 경험한 작은 성공과 실패, 그리고
+            그 속에서 배운 이야기들을 기록한 곳이에요. 누구에게나 실패는
+            아프지만, 그 속에 숨어 있는 배움이 더 큰 도약으로 이어질 수 있다고
+            믿어요.
           </PageHeader.Text>
           <br />
           <PageHeader.Text>
@@ -49,8 +53,8 @@ export default async function Home() {
             </Link>
             에서 Product engineer로 일하고 있어요.
             <br />
-            보다 편리하고 단순하게 만드는 것에 열광해요. 그리고 오래오래 Long
-            run 하기 위해 노력해요.
+            보다 편리하고 단순하게 만드는 것에 열광하며, 오래도록 지속 가능한
+            삶과 일을 위해 노력하고 있습니다.
           </PageHeader.Text>
         </div>
       </PageHeader>
--- components/layout/PageHeader.tsx
+++ components/layout/PageHeader.tsx
@@ -1,10 +1,10 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Divider } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface Props {
   title: string;
-  description: string;
-  children: ReactNode;
+  description: ReactNode;
+  children?: ReactNode;
 }
 
 PageHeader.Text = PageHeaderText;
@@ -13,11 +13,12 @@ export function PageHeader({ title, description, children }: Props) {
     <Box as="header" className="w-full flex flex-col gap-8">
       <Box className="flex flex-col gap-2">
         <Heading size="sm">{title}</Heading>
-        <Text fontSize={15} fontWeight={300}>
+        <Text fontSize={14} fontWeight={300}>
           {description}
         </Text>
       </Box>
       {children}
+      <Divider variant="dashed" />
     </Box>
   );
 }
