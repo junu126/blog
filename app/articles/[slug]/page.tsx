03b36790dfd0a8f1edf6a39e029f863bc3899cca
flex-junwoo <junwoo@flex.team>
2024-12-25 18:53:57 +0900

    feat(post): 날짜 표기

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { PageHeader } from "@/components/layout/PageHeader";
 import { PostBody } from "@/components/post/MdxReader";
+import { formatDate } from "@/lib/date";
 import { getPost } from "@/lib/post";
 import type { Metadata } from "next";
 
@@ -20,7 +21,8 @@ export default async function Post(props: {
     <div className="w-full flex flex-col justify-center" style={{ gap: 28 }}>
       <PageHeader title={post.title} description={post.description}>
         <PageHeader.Text size="small">
-          Read time: {post.readingMinutes}분
+          {formatDate(post.modifiedAt, "yyyy년 MM월 dd일")}
+          &nbsp;&nbsp;|&nbsp;&nbsp; Read time: {post.readingMinutes}분
         </PageHeader.Text>
       </PageHeader>
       <PostBody content={post.content} />
--- app/thoughts/[slug]/page.tsx
+++ app/thoughts/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { PageHeader } from "@/components/layout/PageHeader";
 import { PostBody } from "@/components/post/MdxReader";
+import { formatDate } from "@/lib/date";
 import { getPost } from "@/lib/post";
 import type { Metadata } from "next";
 
@@ -17,19 +18,11 @@ export default async function Post(props: {
   const post = await getPost(slug);
 
   return (
-    <div
-      className="w-full"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 28,
-      }}
-    >
+    <div className="w-full flex flex-col justify-center" style={{ gap: 28 }}>
       <PageHeader title={post.title} description={post.description}>
         <PageHeader.Text size="small">
-          Read time: {post.readingMinutes}분
+          {formatDate(post.modifiedAt, "yyyy년 MM월 dd일")}
+          &nbsp;&nbsp;|&nbsp;&nbsp; Read time: {post.readingMinutes}분
         </PageHeader.Text>
       </PageHeader>
       <PostBody content={post.content} />
